47e5ce1d28ad9a2e7a7eef2b0e7738c81ae4e2d7
asloba <aslobaa@mail.ru>
2021-05-19 22:54:08 +0300

    "Json-schema added"

--- src/test/java/ru/netology/rest/MobileBankApiTestV6.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV6.java
@@ -7,6 +7,7 @@ import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 class MobileBankApiTestV6 {
     private RequestSpecification requestSpec = new RequestSpecBuilder()
@@ -28,7 +29,9 @@ class MobileBankApiTestV6 {
       .when()
           .get("/demo/accounts")
       // Проверки
-      .then()
-          .statusCode(200);
+              .then()
+              .statusCode(200)
+              .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+      ;
     }
 }
